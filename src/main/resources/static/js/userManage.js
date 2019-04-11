063406405bd21839bf5810f5b41ed08d390aba99
ZTS-20180529RRW\wb <wangcy@many-it.com>
2019-04-11 15:26:17 +0800

    jqgrid修改

--- src/main/resources/static/js/userManage.js
+++ src/main/resources/static/js/userManage.js
@@ -6,11 +6,11 @@ $(function () {
 function loadJqGrid() {
     $("#users_table").jqGrid({
        caption:"所有用户信息",
-       width:1000,
-       height:100,
+       width:1670,
+       height:200,
        url:"/getUserInfo",
        postData:{"id":userId,"action":"allUser"},
-       dataType:"json",
+       datatype:"json",//小写
        colNames:['id','名字','密码','邮箱','别名'],
        colModel:[
            {name:"id",index:"id",width:0,align:"center"},
@@ -18,12 +18,13 @@ function loadJqGrid() {
            {name:"passWord",index:"passWord",width:200,align:"center"},
            {name:"email",index:"email",width:200,align:"center"},
            {name:"nickName",index:"nickName",width:200,align:"center"},
-           // {name:"regTime",index:"regTime",width:"20%",align:"center"},
+           // {name:"regTime",index:"regTime",width:"20%",align:"center"}
        ],
        autowidth:"true",
        rowNum:10,
        pager:"#pager"
        // sortname:"id",
-       // sortorder:"desc"
+       // sortorder:"desc",
    })
+    // $("#users_table").jqGrid('navGrid', '#pager', {edit : false,add : false,del : false});
 }
